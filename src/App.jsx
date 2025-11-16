3daffac2289d0d220a99f32cd19e6ea2ce04033a
ABDUL KHALIQ ARRASYID <khaliqarrasyidabdul@gmail.com>
2025-11-16 22:13:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   useEffect(() => {
     const checkSession = async () => {
-      const { data: { session } } = await supabase.auth.getSession();
+      const {  { session } } = await supabase.auth.getSession();
       if (session) {
         setUser(session.user);
       }
@@ -25,7 +25,7 @@ function App() {
     };
     checkSession();
 
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+    const {  { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
       setUser(session?.user || null);
     });
 
